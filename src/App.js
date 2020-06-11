620ef9a9caea9a5c94d871810f589ffa51c91ce5
falkune <iboudiokel@gmail.com>
2020-06-11 16:16:09 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Card from './components/Card'
+import Select from "./components/Select";
+import { movies$ } from './Movies/movies';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      movies: [],
+      categories: [],
+      paginationStart: 0,
+      paginationEnd: 4
+    };
+    this.deleteHandler = this.deleteHandler.bind(this);
+  }
+
+  handleClick(event) {
+    this.setState({
+      currentPage: Number(event.target.id)
+    });
+  }
+
+  componentDidMount () {
+    movies$.then(data => {
+      this.setState({
+        movies: data,
+        categories: [...new Set(data.map(entry=>entry.category)).values()]
+      })
+    })
+  }
+
+  deleteHandler (id) {
+    this.setState({
+      movies: this.state.movies.filter(movie => movie.id !== id),
+    },()=>{
+      this.state.categories = [...new Set(this.state.movies.map(entry=>entry.category)).values()]
+    });
+  }
+
+  navigateToPrev = () => {
+    if(this.state.paginationStart > 0){
+      this.setState({
+        paginationStart: this.state.paginationStart -4,
+        paginationEnd: this.state.paginationEnd -4
+      })
+    }
+  }
+
+  navigateToNext = () => {
+    if(this.state.paginationEnd < this.state.movies.length){
+      this.setState({
+        paginationStart: this.state.paginationStart +4,
+        paginationEnd: this.state.paginationEnd +4
+      })
+    }
+  }
+
+  render () {
+    const { movies, paginationStart, paginationEnd } = this.state;
+    return (
+      <div className='container'>
+        <div id="select">
+          <Select categories={this.state.categories} ></Select>
+        </div>
+        <div className='row  flex-container'>
+          {movies.slice(paginationStart, paginationEnd).map(movie => {
+            return (
+              <Card
+                onDelete={this.deleteHandler}
+                key={movie.id}
+                {...movie}
+              />
+            )
+          })}
+        </div>
+        <div className="d-flex justify-content-between">
+          <button className="btn btn-primary" onClick={this.navigateToPrev}>précédent</button>
+          <button className="btn btn-primary" onClick={this.navigateToNext}>suivant</button>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/components/Card.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import DeleteBtn from './Button';
+
+class Card extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      likes: props.likes,
+      dislikes: props.dislikes,
+      liked: false,
+      disliked: false
+    }
+  }
+
+  like(){
+    if(this.state.liked == false && this.state.disliked == false){
+      this.setState(prev=>{
+        return {
+          likes: prev.likes + 1,
+          liked: true
+        }
+      })
+    }
+    if(this.state.liked == false && this.state.disliked == true){
+      this.setState(prev=>{
+        return {
+          likes: prev.likes + 1,
+          dislikes: prev.dislikes - 1,
+          liked: true,
+          disliked: false
+        }
+      })
+    }
+  }
+  dislike() {
+    if(this.state.disliked == false && this.state.liked == false){
+      this.setState(prev=>{
+        return {
+          dislikes: prev.dislikes + 1,
+          disliked: true
+        }
+      })
+    }
+    if(this.state.disliked == false && this.state.liked == true){
+      this.setState(prev=>{
+        return {
+          likes: prev.likes - 1,
+          dislikes: prev.dislikes + 1,
+          liked: false,
+          disliked: true
+        }
+      })
+    }
+  }
+
+  render () {
+    return (
+      <div className='col-sm-10 col-md-5 col-lg-6' id="item">
+        <div className='card'>
+          <DeleteBtn class="btn btn--delete" click={this.props.onDelete.bind(this,this.props.id)}>✗</DeleteBtn>
+          <h1 className='card__title'>{this.props.title}</h1>
+          <p className="card__subtitle" >
+            {this.props.category}
+          </p>
+
+          <div className='card__footer'>
+            <div>
+              <span className="like" onClick={this.like.bind(this)}><i className="far fa-thumbs-up"></i></span> {this.state.likes} Likes 
+            </div>
+            <div>
+              <span className="dislike" onClick={this.dislike.bind(this)}><i className="far fa-thumbs-down"></i></span> {this.state.dislikes} Dislikes
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Card
